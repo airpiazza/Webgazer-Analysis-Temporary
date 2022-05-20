7c34c75f6bba03414881686209e91a4ebe01b658
airpiazza <nick@piazzaonline.com>
2022-05-20 10:13:14 -0700

    new url

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: `http://webgazeranalysisbackend-env.eba-px8exstr.us-west-1.elasticbeanstalk.com/`
+    baseURL: `https://server.webgazerbackend.link/`
     // baseURL: `http://54.193.20.125:8081/` `https://backend.cpphappiwebapp.com/`
   })
 }
